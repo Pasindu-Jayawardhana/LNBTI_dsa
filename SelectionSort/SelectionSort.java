fcbda950f3c9fb2e6b8af9a85abf470947559115
Pasindu-Jayawardhana <sadhanjanajayawardhana@gmail.com>
2024-06-06 19:33:47 +0530

    commit

---
+++ SelectionSort/SelectionSort.java
@@ -0,0 +1,43 @@
+public class SelectionSort {
+
+    public static void main(String[] args) {
+        int[] array = {64, 25, 12, 22, 11};
+
+        System.out.println("Original array:");
+        printArray(array);
+
+        selectionSort(array);
+
+        System.out.println("Sorted array:");
+        printArray(array);
+    }
+
+    // Function to perform selection sort
+    public static void selectionSort(int[] array) {
+        int n = array.length;
+
+        // One by one move the boundary of the unsorted subarray
+        for (int i = 0; i < n - 1; i++) {
+            // Find the minimum element in the unsorted array
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the found minimum element with the first element
+            int temp = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = temp;
+        }
+    }
+
+    // Utility function to print the array
+    public static void printArray(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(array[i] + " ");
+        System.out.println();
+    }
+}
